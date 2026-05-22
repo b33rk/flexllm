273c1821a6ed38d11a0f8aa287a97a5f95a1b978
b33rk <brkmanics@gmail.com>
2026-05-22 17:54:45 +0700

    feat; add disable check on llama

--- benchmarking/finetuning/benchmark_llamafactory.sh
+++ benchmarking/finetuning/benchmark_llamafactory.sh
@@ -9,7 +9,7 @@ cd "${SCRIPT_DIR}/../../LLaMA-Factory"
 # rm -rf saves
 
 # Single GPU LLAMA-3.1 8B
-CUDA_VISIBLE_DEVICES=0 llamafactory-cli train examples/flexllm/t1_llama_8B.yaml
+CUDA_VISIBLE_DEVICES=0 DISABLE_VERSION_CHECK=1 llamafactory-cli train examples/flexllm/t1_llama_8B.yaml
 
 rm -rf ../output/llama-factory || true
 mkdir -p ../output/llama-factory
